f311054812fdc44b8bd3ac57e73262ac0c842f0a
VK <DEFCONone1@users.noreply.github.com>
2018-02-12 19:06:54 +0000

    Adds working login functionality

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
 
 	domain = "http://localhost:1337";
+	authToken;
+	user;
+	options;
 
 	constructor(
 		private http: Http
 	) {
 
+	}
+
+	createAuthenticationHeaders() {
+		this.loadToken();
+
+		this.options = new RequestOptions({
+			headers: new Headers({
+				'Content-Type': 'application/json',
+				'authorization': this.authToken,
+			})
+		});
+	}
+
+	loadToken() {
+		this.authToken = localStorage.getItem('token');;
 	}
 
 	registerUser(user) {
@@ -23,5 +42,31 @@ export class AuthService {
 
 	checkEmail(email) {
 		return this.http.get(`${this.domain}/authentication/checkEmail/${email}`).map(res => res.json());
+	}
+
+	login(user) {
+		return this.http.post(`${this.domain}/authentication/login`, user).map(res => res.json());
+	}
+
+	logout() {
+		this.authToken = null;
+		this.user = null;
+		localStorage.clear();
+	}
+
+	storeUserData(token, user) {
+		localStorage.setItem('token', token);
+		localStorage.setItem('user', JSON.stringify(user));
+		this.authToken = token;
+		this.user = user;
+	}
+
+	getProfile() {
+		this.createAuthenticationHeaders();
+		return this.http.get(`${this.domain}/authentication/profile`, this.options).map(res => res.json());
+	}
+
+	loggedIn() {
+		return tokenNotExpired();
 	}
 }
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static(`${__dirname}/client/dist`));
 app.use('/authentication', authentication);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/dist/index.html`));
+    res.sendFile(path.join(`${__dirname}/client/src/index.html`));
 });
 
 app.listen(1337, () => {
