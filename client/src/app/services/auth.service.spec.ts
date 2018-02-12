42353eee1c534e5ec330996d23293464d5826d50
VK <DEFCONone1@users.noreply.github.com>
2018-02-12 23:33:23 +0000

    Fixes unit tests for services

--- client/src/app/services/auth.service.spec.ts
+++ client/src/app/services/auth.service.spec.ts
@@ -4,11 +4,47 @@ import { AuthService } from './auth.service';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { ProjectComponent } from '../components/project/project.component';
+
+import { AppComponent } from '../app.component';
+import { HomeComponent } from '../components/home/home.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { RegisterComponent } from '../components/register/register.component';
+import { LoginComponent } from '../components/login/login.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from '../app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+import { APP_BASE_HREF } from '@angular/common';
+import { AuthGuard } from '../guards/auth.guard';
+import { NotAuthGuard } from '../guards/notAuth.guard';
+import { ProjectService } from './project.service';
+
 describe('AuthService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthService]
-    });
+		declarations: [
+			AppComponent,
+			NavbarComponent,
+			HomeComponent,
+			DashboardComponent,
+			RegisterComponent,
+			LoginComponent,
+			ProfileComponent,
+			ProjectComponent
+		],
+		imports: [
+			HttpModule,
+			AppRoutingModule,
+			ReactiveFormsModule,
+			FlashMessagesModule
+		],
+		providers: [{ provide: APP_BASE_HREF, useValue: '/' }, AuthService, AuthGuard, NotAuthGuard, ProjectService],
+	});
   });
 
   it('should be created', inject([AuthService], (service: AuthService) => {
--- client/src/app/services/project.service.spec.ts
+++ client/src/app/services/project.service.spec.ts
@@ -2,10 +2,46 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { ProjectService } from './project.service';
 
+import { ProjectComponent } from '../components/project/project.component';
+
+import { AppComponent } from '../app.component';
+import { HomeComponent } from '../components/home/home.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { RegisterComponent } from '../components/register/register.component';
+import { LoginComponent } from '../components/login/login.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from '../app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+import { APP_BASE_HREF } from '@angular/common';
+import { AuthGuard } from '../guards/auth.guard';
+import { NotAuthGuard } from '../guards/notAuth.guard';
+import { AuthService } from './auth.service';
+
 describe('ProjectService', () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			providers: [ProjectService]
+			declarations: [
+				AppComponent,
+				NavbarComponent,
+				HomeComponent,
+				DashboardComponent,
+				RegisterComponent,
+				LoginComponent,
+				ProfileComponent,
+				ProjectComponent
+			],
+			imports: [
+				HttpModule,
+				AppRoutingModule,
+				ReactiveFormsModule,
+				FlashMessagesModule
+			],
+			providers: [{ provide: APP_BASE_HREF, useValue: '/' }, AuthService, AuthGuard, NotAuthGuard, ProjectService],
 		});
 	});
 
