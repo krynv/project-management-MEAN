2465c668a1613247e95ee3e9125d06fdb0bce891
VK <DEFCONone1@users.noreply.github.com>
2018-02-14 14:52:55 +0000

    Adds comment functionality

--- routes/projects.js
+++ routes/projects.js
@@ -8,15 +8,15 @@ module.exports = (router) => {
 	router.post('/newProject', (req, res) => {
 
 		if (!req.body.title) {
-			res.json({ success: false, message: 'Project title is required.' });
+			res.json({ success: false, message: 'Project title is required' });
 		} else {
 
 			if (!req.body.body) {
-				res.json({ success: false, message: 'Project body is required.' });
+				res.json({ success: false, message: 'Project body is required' });
 			} else {
 
 				if (!req.body.createdBy) {
-					res.json({ success: false, message: 'Project creator is required.' });
+					res.json({ success: false, message: 'Project creator is required' });
 				} else {
 
 					const project = new Project({
@@ -66,7 +66,7 @@ module.exports = (router) => {
 	router.get('/singleProject/:id', (req, res) => {
 
 		if (!req.params.id) {
-			res.json({ success: false, message: 'No project id was provided' });
+			res.json({ success: false, message: 'No project id provided' });
 		} else {
 
 			Project.findOne({ _id: req.params.id }, (err, project) => {
@@ -108,7 +108,7 @@ module.exports = (router) => {
 	router.put('/updateProject', (req, res) => {
 
 		if (!req.body._id) {
-			res.json({ success: false, message: 'No project id provided' });
+			res.json({ success: false, message: 'Project id is required' });
 		} else {
 
 			Project.findOne({ _id: req.body._id }, (err, project) => {
@@ -206,42 +206,42 @@ module.exports = (router) => {
 
 
 	router.put('/assignToProject', (req, res) => {
-		
+
 		if (!req.body.id) {
-			res.json({ success: false, message: 'No id was provided' }); 
+			res.json({ success: false, message: 'Project id is required' });
 		} else {
-			
+
 			Project.findOne({ _id: req.body.id }, (err, project) => {
-				
+
 				if (err) {
-					res.json({ success: false, message: 'Invalid project id' }); 
+					res.json({ success: false, message: 'Invalid project id' });
 				} else {
-					
+
 					if (!project) {
-						res.json({ success: false, message: 'That project was not found' }); 
+						res.json({ success: false, message: 'That project was not found' });
 					} else {
-						
+
 						User.findOne({ _id: req.decoded.userId }, (err, user) => {
-							
+
 							if (err) {
-								res.json({ success: false, message: 'Something went wrong' }); 
+								res.json({ success: false, message: 'Something went wrong' });
 							} else {
-								
+
 								if (!user) {
-									res.json({ success: false, message: 'Could not authenticate user' }); 
+									res.json({ success: false, message: 'Could not authenticate user' });
 								} else {
 									if (project.assignees.includes(user.username)) {
-										res.json({ success: false, message: 'You are already assigned to this project' }); 
+										res.json({ success: false, message: 'You are already assigned to this project' });
 									} else {
-										
-										project.numberOfAssignees++; 
-										project.assignees.push(user.username); 
-										                 
+
+										project.numberOfAssignees++;
+										project.assignees.push(user.username);
+
 										project.save((err) => {
 											if (err) {
-												res.json({ success: false, message: 'Something went wrong' }); 
+												res.json({ success: false, message: 'Something went wrong' });
 											} else {
-												res.json({ success: true, message: 'Project assigned' }); 
+												res.json({ success: true, message: 'Project assigned' });
 											}
 										});
 									}
@@ -251,6 +251,59 @@ module.exports = (router) => {
 					}
 				}
 			});
+		}
+	});
+
+	router.post('/comment', (req, res) => {
+		
+		if (!req.body.comment) {
+			res.json({ success: false, message: 'A comment message is required' });
+		} else {
+			
+			if (!req.body.id) {
+				res.json({ success: false, message: 'No id provided' }); 
+			} else {
+
+				Project.findOne({ _id: req.body.id }, (err, project) => {
+					
+					if (err) {
+						res.json({ success: false, message: 'Invalid project id' }); 
+					} else {
+						
+						if (!project) {
+							res.json({ success: false, message: 'Project not found' }); 
+						} else {
+							
+							User.findOne({ _id: req.decoded.userId }, (err, user) => {
+								
+								if (err) {
+									res.json({ success: false, message: 'Something went wrong' }); 
+								} else {
+									
+									if (!user) {
+										res.json({ success: false, message: 'User not found' }); 
+									} else {
+										
+										project.comments.push({
+											comment: req.body.comment, 
+											commenter: user.username 
+										});
+										
+										project.save((err) => {
+											
+											if (err) {
+												res.json({ success: false, message: 'Something went wrong' }); 
+											} else {
+												res.json({ success: true, message: 'Comment saved' }); 
+											}
+										});
+									}
+								}
+							});
+						}
+					}
+				});
+			}
 		}
 	});
 
